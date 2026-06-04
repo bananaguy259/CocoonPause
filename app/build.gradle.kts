d17ffa5bdd35ec450d6de52650827233b218c8c8
bananaguy259 <bananaguy259@proton.me>
2026-06-04 09:14:48 -0500

    Fix theme with appcompat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.0")
     implementation("androidx.savedstate:savedstate:1.2.1")
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
